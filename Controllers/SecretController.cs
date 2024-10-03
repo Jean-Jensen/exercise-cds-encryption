8fd901c041bb56d27b70207f3c823f80ce592cac
Jean-Jensen <jeanjensen911@gmail.com>
2024-10-03 16:06:20 +0700

    up to "Write text to file"

--- Controllers/SecretController.cs
+++ Controllers/SecretController.cs
@@ -1,3 +1,7 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cds_encryption.Controllers;
@@ -7,7 +11,8 @@ namespace cds_encryption.Controllers;
 public class SecretController : ControllerBase
 {
     private readonly ILogger<SecretController> _logger;
-
+    
+    
     public SecretController(ILogger<SecretController> logger)
     {
         _logger = logger;
@@ -16,6 +21,39 @@ public class SecretController : ControllerBase
     [HttpPost]
     public ActionResult Post([FromBody] SaveSecretMessageRequest request)
     {
+        
+        var salt = RandomNumberGenerator.GetBytes(256 / 8);
+
+        var key = KeyDerivation.Pbkdf2(
+            request.Password,
+            salt,
+            KeyDerivationPrf.HMACSHA256,
+            iterationCount: 600_000,
+            numBytesRequested: 256 /8
+            );
+        
+        var aes = new AesGcm(key);
+        var nonce = new byte[AesGcm.NonceByteSizes.MaxSize]; //MaxSize = 12
+        RandomNumberGenerator.Fill(nonce);
+
+        var plaintextBytes = Encoding.UTF8.GetBytes(request.Message); 
+        var ciphertext = new byte[request.Message.Length];
+        var tag = new byte[AesGcm.TagByteSizes.MaxSize]; // MaxSize = 16
+        
+        aes.Encrypt(nonce, plaintextBytes, ciphertext, tag);
+
+        EncryptedMessage enMes = new EncryptedMessage {Salt = salt, Nonce = nonce, CipherText = ciphertext, Tag = tag};
+        
+        string text = JsonSerializer.Serialize(enMes);
+
+        string docPath = "save files";
+
+        using (StreamWriter outputfile = new StreamWriter(Path.Combine(docPath, "output.txt")))
+        {
+            outputfile.WriteLine(text);
+        }
+        
+        /*
         throw new NotImplementedException(
             @"
             TODO:
@@ -24,6 +62,10 @@ public class SecretController : ControllerBase
             3. Save encrypted message to a file
             "
         );
+        */
+
+        return Ok(text);
+
     }
 
     [HttpGet]
--- Entities/EncryptedMessage.cs
+++ Entities/EncryptedMessage.cs
@@ -4,4 +4,17 @@ public class EncryptedMessage
     public required byte[] Nonce { get; set; }
     public required byte[] CipherText { get; set; }
     public required byte[] Tag { get; set; }
+
+    public EncryptedMessage()
+    {
+        
+    }
+
+    public EncryptedMessage(byte[] salt, byte[] nonce, byte[] cipherText, byte[] tag)
+    {
+        Salt = salt;
+        Nonce = nonce;
+        CipherText = cipherText;
+        Tag = tag;
+    }
 }
